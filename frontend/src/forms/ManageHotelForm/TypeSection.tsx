e8a883b75ae41b7dbcc66ca5da54c634675c916d
samshi27 <samshi.c@gmail.com>
2024-05-16 14:21:35 +0530

    added styling

--- frontend/src/forms/ManageHotelForm/TypeSection.tsx
+++ frontend/src/forms/ManageHotelForm/TypeSection.tsx
@@ -19,8 +19,8 @@ const TypeSection = () => {
           <label
             className={
               typeWatch === type
-                ? "cursor-pointer bg-blue-300 text-sm rounded-full px-4 py-2 font-semibold flex items-center justify-center"
-                : "cursor-pointer bg-gray-300 text-sm rounded-full px-4 py-2 font-semibold flex items-center justify-center"
+                ? "cursor-pointer bg-blue-300 text-sm rounded-full px-4 py-2 font-semibold text-center flex items-center justify-center"
+                : "cursor-pointer bg-gray-300 text-sm rounded-full px-4 py-2 font-semibold text-center flex items-center justify-center"
             }
           >
             <input
